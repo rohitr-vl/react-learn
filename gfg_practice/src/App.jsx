862f050488fc161a44d3b1d99d958de3848a00b9
rohitr-vl <rohitr@vedlogic.com>
2025-03-05 12:30:04 +0530

    web layout components

--- gfg_practice/src/App.jsx
+++ gfg_practice/src/App.jsx
@@ -4,12 +4,32 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 const Navbar = () => {
-  return <h1>This is Navbar.</h1>;
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "0px",
+        backgroundColor: "#41d1ff",
+      }}
+    >
+      <h1>
+        <a>Home</a>
+        <a>Products</a>
+        <a>Services</a>
+        <a>About Us</a>
+        <a>Contact Us</a>
+      </h1>
+    </div>
+  );
 };
 
 // Sidebar Component
 const Sidebar = () => {
-  return <h1>This is Sidebar.</h1>;
+  return (
+    <div style={{ textAlign: "center", marginLeft: "0px", width: "25%" }}>
+      <h1>This is Sidebar.</h1>
+    </div>
+  );
 };
 
 // Body Component
@@ -17,7 +37,7 @@ const Body = () => {
   const [count, setCount] = useState(0);
   return (
     <>
-      <div>
+      <div style={{ textAlign: "justify", marginRight: "0px", width: "75%" }}>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
